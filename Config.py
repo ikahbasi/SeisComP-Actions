8a99c94b6938e6a64cedefa8b13e62e4c156c618
Iman Kahbasi <ikahbasi@gmail.com>
2023-01-26 12:24:18 +0330

    A configuration file that contains all necessary parameters.

---
+++ Config.py
@@ -0,0 +1,12 @@
+# Seiscomp
+class seiscomp_info:
+    def __init__(self):
+        self.mysql_user = 'sysop'
+        self.mysql_pass = 'sysop'
+        self.host       = 'localhost'
+
+# Google Gmail
+class gmail_info:
+    def __init__(self):
+        self.user    = 'example@gmail.com'
+        self.pass    = 'ABCDEFGH'
